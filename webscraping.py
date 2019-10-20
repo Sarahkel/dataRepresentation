d57e59723213ad116f47e008a2dd0065e006630b
sarahkel <G00364736@gmit.ie>
2019-10-20 16:54:11 +0100

    Lab3 - 2.

--- webscraping.py
+++ webscraping.py
@@ -1,7 +1,15 @@
 #Lab 3, Sarah Scholz, GMIT, Webscraping with Python
 
 import requests
+from bs4 import  BeautifulSoup
+
 page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
 print(page)
 print("------------")
 print(page.content)
+print("------------")
+print("------------")
+
+soup1 = BeautifulSoup(page.content, 'html.parser')
+print("------------")
+print(soup1.prettify())
