61fbf80589fddeca7019a224602e7bcefe65ea82
sarahkel <G00364736@gmit.ie>
2019-10-20 17:13:34 +0100

    Lab3 - 4.-7.

--- webscraping.py
+++ webscraping.py
@@ -5,5 +5,11 @@ from bs4 import  BeautifulSoup
 with open("./cars.html") as fp:
 
     soup = BeautifulSoup(fp, 'html.parser')
-print("------------")
-print(soup.prettify())
+rows = soup.findAll("tr")
+for row in rows: 
+    #print(row)
+    cols = row.findAll("td")
+    dataList = []
+    for col in cols: 
+        dataList.append(col.text)
+    print(dataList)
