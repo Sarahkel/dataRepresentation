edee2c63774d73cae5b48084a7874747599a0037
sarahkel <G00364736@gmit.ie>
2019-10-20 16:58:42 +0100

    Lab3 - 3.

--- webscraping.py
+++ webscraping.py
@@ -2,14 +2,8 @@
 
 import requests
 from bs4 import  BeautifulSoup
+with open("./cars.html") as fp:
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-print(page)
+    soup = BeautifulSoup(fp, 'html.parser')
 print("------------")
-print(page.content)
-print("------------")
-print("------------")
-
-soup1 = BeautifulSoup(page.content, 'html.parser')
-print("------------")
-print(soup1.prettify())
+print(soup.prettify())
